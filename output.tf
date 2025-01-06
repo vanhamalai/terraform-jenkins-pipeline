3d60684d1f5eca24f71e833131bf987932ad67d7
vanhamalai <101619706+vanhamalai@users.noreply.github.com>
2025-01-06 12:42:38 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
-output "public_ip" {
- value       = aws_instance.public_instance.public_ip
+output "private_ip" {
+ value       = aws_instance.private_instance.private_ip
  description = "Public IP Address of EC2 instance"
 }
 
 output "instance_id" {
- value       = aws_instance.public_instance.id
+ value       = aws_instance.private_instance.id
  description = "Instance ID"
 }

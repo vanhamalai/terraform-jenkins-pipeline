1d8d4e1945042c37c72a30b69857201a1339d203
vanhamalai <101619706+vanhamalai@users.noreply.github.com>
2025-01-06 12:44:15 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,14 +13,14 @@ variable "aws_secret_key" {
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "ap-south-1"
+  default     = "eu-west-1"
 }
 
 
 variable "ami" {
    type        = string
-   description = "Ubuntu AMI ID"
-   default     = "ami-0f5ee92e2d63afc18"
+   description = "EC2 AMI ID"
+   default     = "ami-0b4dd34260d0315d4"
 }
 
 variable "instance_type" {
